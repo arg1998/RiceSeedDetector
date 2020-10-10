e95eb5f1e2cd4d46337c0b9f7047f8887e42603b
Amir Reza <arg7971@gmail.com>
2020-10-10 21:17:49 +0330

    added comments

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2 as cv
 import utils
 
-STD_THRESHOLD_BLUE = 1.2
-CROP_SIZE = 400
+STD_THRESHOLD_BLUE = 1.2 # STD coefficient for blue image
+CROP_SIZE = 400 # image crop size
+# respective configs for blue and black bg images 
 CONFIGS = {
     "blue": {
         "otsu_threshold": 120,
@@ -21,6 +22,7 @@ CONFIGS = {
     }
 }
 
+# image file configs 
 IMAGE_DATASET_INFO = [
     {
         "background_color": "blue",
@@ -41,13 +43,19 @@ for image_entry in IMAGE_DATASET_INFO:
     _img_background_color = image_entry["background_color"]
     _img_cfg = CONFIGS[_img_background_color]
 
+    # load image 
     original_image = utils.load_image(F"input/{_img_file_name}.{_img_extension}")
-
+    # producing a binary image (OTSU thresholding) after converting the original image into a grey-scale image
     binary_image = utils.binarize_image(original_image, _img_cfg["blur_size"], _img_cfg["blur_sigma"], _img_cfg["otsu_threshold"])
+    # extracting edges from previously generated binary image, using canny edge detection algorithm 
     canny_image = utils.canny(binary_image, _img_cfg["canny_threshold"], _img_cfg["canny_alpha"])
+    # calculating outlines of grains by finding their contours (extracting the most external contour in each hierarchy)
     _, contours, hierarchy = utils.find_contours(canny_image)
+    # creating a white background image to use as a canvas 
     white_canvas = utils.create_image((*canny_image.shape, 3), 255)
+    # drawing grains outlines over the canvas 
     utils.draw_contours(white_canvas, contours, hierarchy)
+    # calculating circumferencing circles and boxes for each contour 
     circles, boxes = utils.get_circles_and_boxes(contours)
 
     contour_image = white_canvas.copy()
@@ -55,22 +63,22 @@ for image_entry in IMAGE_DATASET_INFO:
     utils.draw_circles(white_canvas, circles)
     utils.draw_boxes(white_canvas, boxes)
 
-    _radius = circles[:, 2]  # extract the radius column
-    _min = np.min(_radius)
-    _max = np.max(_radius)
-    _average = np.mean(_radius)
+    # ------------------------- filtering small and large objects ------------------------------------
+    # filtering small and large objects using median and standard deviation over objects radii as a measure
+    _radius = circles[:, 2]  # extract the radii from detected objects' circular boundaries 
     _std = np.std(_radius)
     _median = np.median(_radius)
 
     _refined_circles = None
     _ignored_circles = None
     _mask = None
+    # filtering conditions 
     if _img_background_color == "black":
         _mask = (_median + 25 > _radius) & (_radius > _median - 25)
     else:
-        T = _std * STD_THRESHOLD_BLUE
         _mask = _radius > _median - _std
 
+    # applying mask indices over unfilterd objects
     _refined_circles = circles[_mask]
     _ignored_circles = circles[np.bitwise_not(_mask)]
 
@@ -81,6 +89,7 @@ for image_entry in IMAGE_DATASET_INFO:
     crop_image = contour_image.copy()
     _margin = np.int32(CROP_SIZE / 2)
     _circle_centers = _refined_circles[:, 0:2].astype(np.int32)
+    # calculating crop region coordinations based on _margin
     p1_x = np.subtract(_circle_centers[:, 0], _margin, dtype=np.int32)
     p1_y = np.subtract(_circle_centers[:, 1], _margin, dtype=np.int32)
 
@@ -91,6 +100,7 @@ for image_entry in IMAGE_DATASET_INFO:
     pt2 = np.array(tuple(zip(p2_x, p2_y)), dtype=np.int32)
 
     _image_crop_area = list(zip(pt1, pt2))
+    # drawing crop regions
     utils.draw_boxes(crop_image, _image_crop_area)
 
     utils.write_image(binary_image, F"output/{_img_background_color}/01 - binary.{_img_extension}")
@@ -100,5 +110,6 @@ for image_entry in IMAGE_DATASET_INFO:
     utils.write_image(refined_image, F"output/{_img_background_color}/05 - refined.{_img_extension}")
     utils.write_image(crop_image, F"output/{_img_background_color}/06 - crops.{_img_extension}")
 
+    # saving boundaries in a numpy file for future calculations 
     utils.write_numpy_array(circles, F"output/{_img_background_color}/{_img_file_name}.circles.npy")
     utils.write_numpy_array(boxes, F"output/{_img_background_color}/{_img_file_name}.boxes.npy")
